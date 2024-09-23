5eb561b17cac76027da382ccbdcdb7e473c2c3a8
AleksanderNoszczyk <al.noszczyk@gmail.com>
2024-09-23 10:03:15 +0200

    exit

--- login.php
+++ login.php
@@ -12,7 +12,12 @@
         $result = $query->get_result();
 
         if ($result->num_rows > 0){
-            
+            $user = $result->fetch_assoc();
+            if (password_verify($password, $user['password'])){
+                $_SESSION['user_id'] = $user['id'];
+                header("Location: index.php");
+                exit;
+            }
         }
     }
 ?>
