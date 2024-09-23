d6e2ef832cd15a9fac54386d5694ae99f6d741fb
AleksanderNoszczyk <al.noszczyk@gmail.com>
2024-09-23 10:11:35 +0200

    koniec login

--- login.php
+++ login.php
@@ -25,3 +25,26 @@
         }
     } 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="login-container">
+        <form action="login.php" method="post">
+            <h2>Logowanie</h2>
+            <?php if (isset($error)) echo "<p class='error'>$error</p>"; ?>
+            <label for="username">Nazwa użytkownik</label>
+            <input type="text" id="username" name="username" required>
+            <label for="password">Hasło:</label>
+            <input type="password" id="password" name="password" required>
+            <button type="sumbit">Zaloguj się</button>
+        </form>
+    </div>
+</body>
+</html>
